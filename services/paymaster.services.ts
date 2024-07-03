368c6d084d8811a4d9eac926cf09d129cc2de6d8
Van Hohenheim <0xhohenheim@gmail.com>
2024-07-04 04:48:43 +0530

    feat: paymaster mainnet support

--- services/paymaster.services.ts
+++ services/paymaster.services.ts
@@ -1,11 +1,11 @@
 import { createClient, http } from "viem";
-import { baseSepolia } from "viem/chains";
+import { base, baseSepolia } from "viem/chains";
 import { ENTRYPOINT_ADDRESS_V06 } from "permissionless";
 import { paymasterActionsEip7677 } from "permissionless/experimental";
 
 const paymasterService = process.env.PAYMASTER_SERVICE_URL!;
 
 export const paymasterClient = createClient({
-    chain: baseSepolia,
+    chain: process.env.APP_ENV == "production" ? base : baseSepolia,
     transport: http(paymasterService),
 }).extend(paymasterActionsEip7677(ENTRYPOINT_ADDRESS_V06));
